95abe7173e1aff9bbba7dd4e1d91585bf16fe41b
Miten Chauhan <miten.chauhan@safer.me>
2019-02-20 21:24:30 +1300

    Change design of home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   title = `Austin D'Souza`;
+  subtitle = 'Graduate Architect';
 
   constructor() { }
 
