8b5f12aa82ccc85d4ec7082b98ee4f8995fde921
Dilen Chauhan <dilenchauhan@Dilens-MacBook-Pro.local>
2019-02-23 14:31:31 +1300

    Remove CV from app modeule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AboutComponent } from './about/about.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
-import { CvComponent } from './cv/cv.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { MatToolbarModule, MatGridListModule } from '@angular/material';
@@ -22,7 +21,6 @@ import { SmoothScrollDirective } from './smooth-scroll.directive';
     AboutComponent,
     NavigationComponent,
     PortfolioComponent,
-    CvComponent,
     ContactComponent,
     HomeComponent,
     ProjectComponent,
