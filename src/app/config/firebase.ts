bdedc6d6d17ba633f0f657dacc552679ab9ddb02
Miten Chauhan <miten.chauhan@safer.me>
2019-03-22 23:50:16 +1300

    Integrate firebase into project

---
+++ src/app/config/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAjJAmS8wwFimcYxUVaF-7aURAv09sGNDM",
+  authDomain: "parthenon-ad-architecture.firebaseapp.com",
+  databaseURL: "https://parthenon-ad-architecture.firebaseio.com",
+  projectId: "parthenon-ad-architecture",
+  storageBucket: "parthenon-ad-architecture.appspot.com",
+  messagingSenderId: "330750359103"
+};
