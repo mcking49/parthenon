11ecfac61a24b66a3cd7b94e6008e71d766a91b4
Miten Chauhan <miten.chauhan@safer.me>
2019-03-23 00:35:04 +1300

    IMPLEMENT DOWNLOAD CV FUNCTIONALITY

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from './../storage.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private storage: StorageService) { }
 
   ngOnInit() {
+  }
+
+  downloadCv() {
+    this.storage.downloadCv();
   }
 
 }
---
+++ src/app/storage.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  private storageRef: firebase.storage.Reference;
+
+  constructor() {
+    this.initialiseFirebaseStorage();
+  }
+
+  public downloadCv() {
+    this.cvRef.getDownloadURL()
+      .then((url) => {
+        window.open(url, '_blank');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  private initialiseFirebaseStorage() {
+    this.storageRef = firebase.storage().ref();
+  }
+
+  get cvRef() {
+    return this.storageRef.child('cv/Dsouza_Austin-CV19-Web.pdf');
+  }
+}
