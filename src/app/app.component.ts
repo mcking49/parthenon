6d9ee3d9a928bc646372cd64f312c2fa50c77eac
Miten Chauhan <miten.chauhan@safer.me>
2019-03-30 21:39:21 +1300

    fix firebase import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
 import { firebaseConfig } from './config/firebase';
 
 @Component({
