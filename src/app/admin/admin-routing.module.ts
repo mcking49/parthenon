70afc17215b98975b05596b0bfeb95de02c33cce
Miten Chauhan <miten.chauhan@safer.me>
2019-04-12 08:02:47 +1200

    Create basic auth implementation

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,9 +1,34 @@
-import { AdminComponent } from './admin.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AdminComponent } from './admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+
+import { AuthGuard } from './../guards/auth.guard';
+
 const routes: Routes = [
-  { path: '', component: AdminComponent }
+  {
+    path: '',
+    component: AdminComponent,
+    pathMatch: 'prefix',
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
