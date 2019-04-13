665d5159a50c5f9816b0bf08d5821a1524a8b33b
Miten Chauhan <miten.chauhan@safer.me>
2019-04-14 11:32:02 +1200

    Disable side menu for the login page

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,3 +1,4 @@
+import { Router, NavigationEnd } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ResponsiveService } from '../services/responsive.service';
@@ -10,11 +11,26 @@ import { ResponsiveService } from '../services/responsive.service';
 export class AdminComponent implements OnInit {
 
   public isHandset$: Observable<boolean>;
+  public isLoginPage: boolean;
+  private readonly loginUrl = '/admin/login';
 
-  constructor(private responsiveService: ResponsiveService) {}
+  constructor(
+    private responsiveService: ResponsiveService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.isHandset$ = this.responsiveService.isHandset;
+
+    // initialise the isLoginPage field.
+    this.isLoginPage = this.router.routerState.snapshot.url === this.loginUrl
+
+    // Subscribe to the Router events to update isLoginPage when the URL changes.
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isLoginPage = event.url === this.loginUrl;
+      }
+    });
   }
 
 }
