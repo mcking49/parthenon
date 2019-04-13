c8bce5c94a3a3f1df81abc282c18cd73db4d0e88
Miten Chauhan <miten.chauhan@safer.me>
2019-04-13 14:47:09 +1200

    Fix breakages from rebasing

--- src/app/austin/austin-routing.module.ts
+++ src/app/austin/austin-routing.module.ts
@@ -1,8 +1,9 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AustinComponent } from './austin.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectComponent } from './project/project.component';
+import { ThesisComponent } from './thesis/thesis.component';
 
 const routes: Routes = [
   {
@@ -12,7 +13,9 @@ const routes: Routes = [
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
-      { path: 'project/:url', component: ProjectComponent }
+      { path: 'project/:url', component: ProjectComponent },
+      { path: 'master-thesis/2019-the-togetherness-of-strangers', component: ThesisComponent},
+      { path: 'thesis', redirectTo: 'master-thesis/2019-the-togetherness-of-strangers', pathMatch: 'full' }
     ]
   }
 ];
