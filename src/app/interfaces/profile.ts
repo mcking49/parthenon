66b552e2a3ddcdbcbd89203557a902c0e8ffc9d6
Miten Chauhan <miten.chauhan@safer.me>
2019-04-18 20:45:35 +1200

    Create core profile service

---
+++ src/app/interfaces/profile.ts
@@ -0,0 +1,10 @@
+export interface Profile {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  linkedInUrl: string;
+  cvEnUrl: string;
+  cvDeUrl: string;
+  thesisUrl: string;
+}
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/firestore';
+import { AngularFireStorage, AngularFireStorageReference } from '@angular/fire/storage';
+import * as firebase from 'firebase/app';
+import { Profile } from 'src/app/interfaces/profile';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private profileDoc: AngularFirestoreDocument<Profile>;
+  private profile$: Observable<Profile>;
+
+  constructor(
+    private afStore: AngularFirestore
+  ) {
+    this.profileDoc = this.afStore.doc<Profile>('profile/1');
+    this.profile$ = this.profileDoc.valueChanges();
+    this.getProfile();
+  }
+
+  /**
+   * Get the profile.
+   *
+   * @returns {Observable<Profile>} - The user's profile.
+   */
+  public getProfile(): Observable<Profile> {
+    return this.profile$;
+  }
 }
