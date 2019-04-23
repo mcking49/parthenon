ff5d8f228fcafefa62c5385847898c0c9b537f41
Miten Chauhan <miten.chauhan@safer.me>
2019-04-23 22:25:21 +1200

    Add stronger validation to cv upload form

--- src/app/admin/cv/cv.component.ts
+++ src/app/admin/cv/cv.component.ts
@@ -1,6 +1,7 @@
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from './../../services/storage.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-cv',
@@ -11,6 +12,7 @@ export class CvComponent implements OnInit {
 
   public selectedFile: File;
   public cvForm: FormGroup;
+  public isEditingMode: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -21,8 +23,27 @@ export class CvComponent implements OnInit {
     this.initialiseForm();
   }
 
+  public get isFormDisabled(): boolean {
+    return !this.isEditingMode || !this.cvForm.valid;
+  }
+
+  public get isUploadDisabled(): boolean {
+    return !this.isEditingMode;
+  }
+
   public newCvSelected(files: FileList) {
     this.selectedFile = files.item(0);
+  }
+
+  public toggleEditingState() {
+    this.isEditingMode = !this.isEditingMode;
+    if (this.isEditingMode) {
+      _.each(this.cvForm.controls, (value: any, key: string) => {
+        this.cvForm.controls[key].enable();
+      });
+    } else {
+      this.resetForm();
+    }
   }
 
   public uploadCv() {
@@ -36,13 +57,31 @@ export class CvComponent implements OnInit {
   private initialiseForm() {
     this.cvForm = this.formBuilder.group({
       cv: [
-        null,
+        {
+          value: null,
+          disabled: !this.isEditingMode,
+        },
         Validators.required
       ],
       language: [
-        '',
+        {
+          value: '',
+          disabled: !this.isEditingMode,
+        },
         Validators.required
       ]
+    });
+  }
+
+  /**
+   * Reset the form to it's initial states with default values.
+   */
+  private resetForm(): void {
+    this.cvForm.reset();
+    this.selectedFile = null;
+    _.each(this.cvForm.controls, (value: any, key: string) => {
+      this.cvForm.controls[key].setValue('');
+      this.cvForm.controls[key].disable();
     });
   }
 
