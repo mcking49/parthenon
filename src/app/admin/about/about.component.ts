4071dade8544fbfcb7ae8dc80c8d35407ca0bc61
Miten Chauhan <miten.chauhan@safer.me>
2019-04-25 20:29:09 +1200

    Implement core about section with dynamic forms

--- src/app/admin/about/about.component.ts
+++ src/app/admin/about/about.component.ts
@@ -1,11 +1,11 @@
-import { AboutService } from './../../services/about.service';
-import { MatDialog, MatSnackBar } from '@angular/material';
+// import { AboutService } from './../../services/about.service';
+// import { MatDialog, MatSnackBar } from '@angular/material';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+// import { Observable } from 'rxjs';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import { LoadingSpinnerModalComponent } from 'src/app/components/loading-spinner-modal/loading-spinner-modal.component';
+// import { LoadingSpinnerModalComponent } from 'src/app/components/loading-spinner-modal/loading-spinner-modal.component';
 import * as _ from 'lodash';
-import { Profile } from 'src/app/interfaces/profile';
+// import { Profile } from 'src/app/interfaces/profile';
 
 @Component({
   selector: 'app-about',
@@ -16,54 +16,68 @@ export class AboutComponent implements OnInit {
 
   public isEditingMode: boolean = false;
   public aboutForm: FormGroup;
+  // public aboutFormControlKeys: string[];
 
   constructor(
-    private dialog: MatDialog,
+    // private dialog: MatDialog,
     private formBuilder: FormBuilder,
-    private aboutService: AboutService,
-    private snackbar: MatSnackBar
+    // private aboutService: AboutService,
+    // private snackbar: MatSnackBar
   ) { }
 
   ngOnInit() {
     this.initialiseForm();
   }
 
+  public get aboutFormControlKeys(): string[] {
+    return Object.keys(this.aboutForm.controls);
+  }
+
   public get isDisabled(): boolean {
     return !this.isEditingMode || !this.aboutForm.valid || this.aboutForm.pristine;
   }
 
-  // private async submitForm() {
-  //   // if (this.aboutForm.valid) {
-  //   //   const dialogRef = this.dialog.open(LoadingSpinnerModalComponent, {
-  //   //     maxHeight: '150px',
-  //   //     height: '150px',
-  //   //     width: '150px'
-  //   //   });
+  private async submitForm() {
+    // if (this.aboutForm.valid) {
+    //   const dialogRef = this.dialog.open(LoadingSpinnerModalComponent, {
+    //     maxHeight: '150px',
+    //     height: '150px',
+    //     width: '150px'
+    //   });
 
-  //   //   let updatedProfile = {};
-  //   //   _.each(this.aboutForm.controls, (formControl: FormControl, key: string) => {
-  //   //     updatedProfile[key] = formControl.value;
-  //   //   });
+    //   let updatedProfile = {};
+    //   _.each(this.aboutForm.controls, (formControl: FormControl, key: string) => {
+    //     updatedProfile[key] = formControl.value;
+    //   });
 
-  //   //   try {
-  //   //     await this.aboutService.updateProfile(updatedProfile as Profile);
-  //   //     this.toggleEditingState();
-  //   //     this.snackbar.open(
-  //   //       'The changes have been saved',
-  //   //       'Close',
-  //   //       {
-  //   //         duration: 3000,
-  //   //       }
-  //   //     );
-  //   //   } catch (error) {
-  //   //     console.error(error);
-  //   //   } finally {
-  //   //     dialogRef.close();
-  //   //   }
-  //   // } else {
-  //   //   console.error('Form is invalid.');
-  //   // }
-  // }
+    //   try {
+    //     await this.aboutService.updateProfile(updatedProfile as Profile);
+    //     this.toggleEditingState();
+    //     this.snackbar.open(
+    //       'The changes have been saved',
+    //       'Close',
+    //       {
+    //         duration: 3000,
+    //       }
+    //     );
+    //   } catch (error) {
+    //     console.error(error);
+    //   } finally {
+    //     dialogRef.close();
+    //   }
+    // } else {
+    //   console.error('Form is invalid.');
+    // }
+  }
+
+  public addParagraphSection(): void {
+    const paragraphId: number = new Date().getTime();
+    this.aboutForm.addControl(`paragraph${paragraphId}`, new FormControl('', Validators.required));
+  }
+
+  public deleteParagraphSection(formControlName: string): void {
+    this.aboutForm.removeControl(formControlName);
+  }
 
   public toggleEditingState() {
     this.isEditingMode = !this.isEditingMode;
@@ -78,7 +92,7 @@ export class AboutComponent implements OnInit {
 
   private initialiseForm(): void {
     this.aboutForm = this.formBuilder.group({
-      firstName: [
+      paragraph1: [
         {
           value: '',
           disabled: !this.isEditingMode,
