1ae78aa57bd957a99430ae9ba3a917d0424ec2f4
Miten Chauhan <mitenchauhan@hotmail.com>
2019-05-03 18:33:18 +1200

    Create basic formgroup

--- src/app/admin/projects/projects.component.ts
+++ src/app/admin/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectsService } from 'src/app/services/projects.service';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-projects',
@@ -8,9 +9,38 @@ import { ProjectsService } from 'src/app/services/projects.service';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor(private projectsService: ProjectsService) { }
+  public projectsForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private projectsService: ProjectsService
+  ) { }
 
   ngOnInit() {
+    this.initialiseForm();
+  }
+
+  private initialiseForm(): void {
+    this.projectsForm = this.formBuilder.group({
+      title: [
+        '',
+        Validators.required
+      ],
+      year: [
+        '',
+        Validators.required
+      ],
+      category: [
+        '',
+        Validators.required
+      ],
+      brief: this.formBuilder.array([
+        this.formBuilder.control(
+          '',
+          Validators.required
+        )
+      ]),
+    });
   }
 
 }
