234692113dd0d87ccec6b3a9adaf3a79b30f51a9
Miten Chauhan <mitenchauhan@hotmail.com>
2019-05-19 13:15:38 +1200

    Add loading animation to portfolio

--- src/app/austin/portfolio/portfolio.component.ts
+++ src/app/austin/portfolio/portfolio.component.ts
@@ -3,9 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MatDialog } from '@angular/material';
 import { Observable } from 'rxjs';
-import { StorageService } from 'src/app/services/storage.service';
 import { ProjectsService } from 'src/app/services/projects.service';
-import { LoadingSpinnerModalComponent } from '../../components/loading-spinner-modal/loading-spinner-modal.component';
 import { Projects } from 'src/app/interfaces/projects';
 import { Project } from 'src/app/interfaces/project';
 import * as _ from 'lodash';
@@ -24,17 +22,23 @@ export class PortfolioComponent implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private dialog: MatDialog,
     private projectsService: ProjectsService,
     private responsiveService: ResponsiveService,
-    private router: Router,
-    private storage: StorageService
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.initProjects();
-    this.showLoading = false;
     this.isHandset = this.responsiveService.isHandset;
+  }
+
+  /**
+   * Check if the projects are still loading.
+   *
+   * @returns {boolean} - Returns true if the projects haven't been loaded yet.
+   */
+  public get isLoading(): boolean {
+    return !this.projects;
   }
 
   /**
@@ -54,27 +58,6 @@ export class PortfolioComponent implements OnInit {
       ['../master-thesis/2019-the-togetherness-of-strangers'],
       {relativeTo: this.activatedRoute}
     );
-  }
-
-  /**
-   * Download the Thesis from the database.
-   */
-  public async downloadThesis() {
-    this.showLoading = true;
-    const dialogRef = this.dialog.open(LoadingSpinnerModalComponent, {
-      maxHeight: '150px',
-      height: '150px',
-      width: '150px',
-    });
-
-    try {
-      await this.storage.downloadThesis();
-    } catch (error) {
-      throw error;
-    } finally {
-      dialogRef.close();
-      this.showLoading = false;
-    }
   }
 
   /**
