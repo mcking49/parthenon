f6efce0c316f32b32fe73830b8b2845244e13f5b
Miten Chauhan <mitenchauhan@hotmail.com>
2019-05-21 22:45:16 +1200

    Create the account form

---
+++ src/app/validators/confirm-password-validator.spec.ts
@@ -0,0 +1,7 @@
+import { ConfirmPasswordValidator } from './confirm-password-validator';
+
+describe('ConfirmPasswordValidator', () => {
+  it('should create an instance', () => {
+    expect(new ConfirmPasswordValidator()).toBeTruthy();
+  });
+});
