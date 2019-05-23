e27635b4da17d34d056ba17b126ff7d5cf3c833f
Miten Chauhan <mitenchauhan@hotmail.com>
2019-05-23 22:22:27 +1200

    fix portfolio spec

--- src/app/austin/portfolio/portfolio.component.spec.ts
+++ src/app/austin/portfolio/portfolio.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import {
+  MatCardModule,
+  MatListModule,
+  MatGridListModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { PortfolioComponent } from './portfolio.component';
+import { firebaseConfig } from 'src/app/config/firebase';
 
 describe('PortfolioComponent', () => {
   let component: PortfolioComponent;
@@ -8,7 +18,16 @@ describe('PortfolioComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PortfolioComponent ]
+      declarations: [ PortfolioComponent ],
+      imports: [
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFirestoreModule,
+        MatCardModule,
+        MatGridListModule,
+        MatListModule,
+        MatProgressSpinnerModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
