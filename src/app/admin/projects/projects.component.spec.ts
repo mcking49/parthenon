391e5b1043171767246f2cc601a95af646fbab91
Miten Chauhan <mitenchauhan@hotmail.com>
2019-05-23 22:06:27 +1200

    fix projects component test

--- src/app/admin/projects/projects.component.spec.ts
+++ src/app/admin/projects/projects.component.spec.ts
@@ -1,4 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import {
+  MatCheckboxModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatPaginatorModule,
+  MatTableModule,
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { firebaseConfig } from 'src/app/config/firebase';
 
 import { ProjectsComponent } from './projects.component';
 
@@ -8,7 +22,20 @@ describe('ProjectsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProjectsComponent ]
+      declarations: [ ProjectsComponent ],
+      imports: [
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFirestoreModule,
+        AngularFireStorageModule,
+        BrowserAnimationsModule,
+        MatCheckboxModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatPaginatorModule,
+        MatTableModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
