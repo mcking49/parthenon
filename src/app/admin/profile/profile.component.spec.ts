5854e01fb63bf3297556f5c35f01ccd7d45f24b4
Miten Chauhan <mitenchauhan@hotmail.com>
2019-05-23 21:57:35 +1200

    fix profile component spec

--- src/app/admin/profile/profile.component.spec.ts
+++ src/app/admin/profile/profile.component.spec.ts
@@ -1,4 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  MatDialogModule,
+  MatIconModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatSlideToggleModule,
+  MatSnackBarModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { firebaseConfig } from 'src/app/config/firebase';
 
 import { ProfileComponent } from './profile.component';
 
@@ -8,7 +22,20 @@ describe('ProfileComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfileComponent ]
+      declarations: [ ProfileComponent ],
+      imports: [
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFirestoreModule,
+        AngularFireStorageModule,
+        BrowserAnimationsModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatSlideToggleModule,
+        MatSnackBarModule,
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
   }));
