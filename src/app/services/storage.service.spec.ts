227073a467f0d4bdf6db35cb0e965bb50e4db3de
Miten Chauhan <mitenchauhan@hotmail.com>
2019-05-23 21:15:51 +1200

    fix storage service spec

--- src/app/services/storage.service.spec.ts
+++ src/app/services/storage.service.spec.ts
@@ -1,9 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { StorageService } from './storage.service';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireModule } from '@angular/fire';
+import { firebaseConfig } from '../config/firebase';
 
 describe('StorageService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      AngularFireModule.initializeApp(firebaseConfig),
+      AngularFireStorageModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: StorageService = TestBed.get(StorageService);
