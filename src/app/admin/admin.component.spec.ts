cf4a78efd7198dd82c5e856a1242ac2dba187964
Miten Chauhan <mitenchauhan@hotmail.com>
2019-05-23 21:46:07 +1200

    fix admin component test

--- src/app/admin/admin.component.spec.ts
+++ src/app/admin/admin.component.spec.ts
@@ -1,13 +1,19 @@
 import { LayoutModule } from '@angular/cdk/layout';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import {
   MatButtonModule,
+  MatDialogModule,
   MatIconModule,
   MatListModule,
   MatSidenavModule,
+  MatSlideToggleModule,
   MatToolbarModule,
 } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { firebaseConfig } from '../config/firebase';
 
 import { AdminComponent } from './admin.component';
 
@@ -19,13 +25,18 @@ describe('AdminComponent', () => {
     TestBed.configureTestingModule({
       declarations: [AdminComponent],
       imports: [
-        NoopAnimationsModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFireAuthModule,
         LayoutModule,
         MatButtonModule,
+        MatDialogModule,
         MatIconModule,
         MatListModule,
         MatSidenavModule,
+        MatSlideToggleModule,
         MatToolbarModule,
+        NoopAnimationsModule,
+        RouterTestingModule
       ]
     }).compileComponents();
   }));
