880ea6557a7735d59b8775823fb0f2e60414a0fb
Miten Chauhan <mitenchauhan@hotmail.com>
2019-05-23 21:16:58 +1200

    Fix tracking service spec

--- src/app/services/tracking.service.spec.ts
+++ src/app/services/tracking.service.spec.ts
@@ -1,9 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TrackingService } from './tracking.service';
+import { AngularFireModule } from '@angular/fire';
+import { firebaseConfig } from '../config/firebase';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
-describe('TrackingService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('TrackingService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      AngularFireModule.initializeApp(firebaseConfig),
+      AngularFirestoreModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: TrackingService = TestBed.get(TrackingService);
