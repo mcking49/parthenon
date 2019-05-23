aad43cd4d9c7766e14618966e03d3acc27332c27
Miten Chauhan <mitenchauhan@hotmail.com>
2019-05-23 22:24:37 +1200

    fix project component spec

--- src/app/austin/project/project.component.spec.ts
+++ src/app/austin/project/project.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectComponent } from './project.component';
+import { MatProgressSpinnerModule } from '@angular/material';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
+import { firebaseConfig } from 'src/app/config/firebase';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ProjectComponent', () => {
   let component: ProjectComponent;
@@ -8,7 +14,14 @@ describe('ProjectComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProjectComponent ]
+      declarations: [ ProjectComponent ],
+      imports: [
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFirestoreModule,
+        MatProgressSpinnerModule,
+        NgbCarouselModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
