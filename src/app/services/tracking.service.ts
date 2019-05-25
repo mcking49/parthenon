00a6752f956b58eff72d7f149366504044e2d491
Miten Chauhan <miten.chauhan@safer.me>
2019-05-25 16:32:51 +1200

    Track button click event

--- src/app/services/tracking.service.ts
+++ src/app/services/tracking.service.ts
@@ -16,6 +16,7 @@ declare const gtag: Function;
 export class TrackingService {
 
   public analytics: Subscription;
+  private trackingId: string;
 
   private buttonTrackingDoc: AngularFirestoreDocument<ButtonTracker>;
   private buttonTracking: BehaviorSubject<ButtonTracker> = new BehaviorSubject<ButtonTracker>(null);
@@ -29,6 +30,7 @@ export class TrackingService {
     this.buttonTrackingDoc.valueChanges().subscribe((buttonTrackers: ButtonTracker) => {
       this.buttonTracking.next(buttonTrackers);
     });
+    this.trackingId = environment.googleAnalytics.trackingId;
   }
 
   /**
@@ -36,11 +38,10 @@ export class TrackingService {
    */
   public start(): void {
     if (!this.analytics) {
-      const trackingId = environment.googleAnalytics.trackingId;
       this.analytics = this.router.events.subscribe((event) => {
         if (event instanceof NavigationEnd) {
           try {
-            gtag('config', trackingId, {'page_path': event.urlAfterRedirects});
+            gtag('config', this.trackingId, {'page_path': event.urlAfterRedirects});
           } catch (error) {
             console.error(error);
           }
@@ -71,6 +72,14 @@ export class TrackingService {
   public trackButton(buttonName: ButtonName): Promise<void> {
     const buttonTrackers: ButtonTracker = this.buttonTrackers;
     buttonTrackers[buttonName]++;
+    try {
+      gtag('event', 'buttonClick', {
+        'event_category': 'Button Click',
+        'event_label': buttonName
+      });
+    } catch (error) {
+      console.error(error);
+    }
     return this.buttonTrackingDoc.update(buttonTrackers);
   }
 
